269cb9f4b99cbf52810dc4af914caf5b48d53c3c
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-09-17 23:56:45 +0300

    add virtual environment, updated views.py file and config.py file

---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+from flask import Flask, app
+
+#Initializing application
+app = Flask(__name__)
+from app import views
---
+++ config.py
@@ -0,0 +1,36 @@
+import os
+
+from flask import config
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    pass
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+    'production':ProdConfig,
+    'development':DevConfig
+}
