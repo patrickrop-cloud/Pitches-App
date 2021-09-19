a9bc0f52cd036bf141d3bddc2fe24ea6f660e45c
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-09-20 00:15:13 +0300

    create migrations and debugged the files.

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://patrick:patrick123@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://patrick:patrick@localhost/pitch'
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     #email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
@@ -18,14 +18,13 @@ class Config:
 # class ProdConfig(Config):
 #     pass
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    # if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-    #     SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://")
-
+  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
+  if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",1)
 
 class DevConfig(Config):
-    DEBUG = True
-
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://patrick:patrick@localhost/pitch'
+  DEBUG = True
 config_options = {
 'development':DevConfig,
 'production':ProdConfig
