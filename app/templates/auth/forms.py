dfd23638b40c8227ed1d01876d7bda64c0f10f02
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-09-19 17:29:49 +0300

    update macro and pitch templates files.

--- app/templates/auth/forms.py
+++ app/templates/auth/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 # from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import Required,Email,EqualTo
-from ..models import User
+from models import User
 from wtforms import ValidationError
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
 
